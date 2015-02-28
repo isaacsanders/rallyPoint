8aa574a40df22a0b3c2c26ecfc70fa91d377b87c
John McCormack <mccormjt@gmail.com>
2015-02-28 03:32:25 -0600

    more home page js support and added util logging function

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,8 +1,26 @@
+var self,
+    FINDING_CLASS = 'finding';
+
+Template.home.created = function() {
+    self = this;
+}
+
+Template.home.rendered = function() {
+    self.finder = self.$('.find-group'),
+    self.input  = self.$('input');
+}
+ 
 Template.home.events({
-    'click button': findGroupMemebers 
+    'mousedown button': findGroupMemebers,
+    'mouseup button, mouseout button': stopFindingGroupMembers
 });
 
 
 function findGroupMemebers() {
-    
+    if (!self.input.val()) return 
+    self.finder.addClass(FINDING_CLASS);
+}
+
+function stopFindingGroupMembers() {
+    self.finder.removeClass(FINDING_CLASS);
 }
