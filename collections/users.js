7e5bc1c8f58af4587271a5a21fd33bebeeb520cc
Seth Ruji <someoneseth@gmail.com>
2015-02-28 03:44:20 -0600

    adding user location property

--- collections/users.js
+++ collections/users.js
@@ -4,6 +4,7 @@ Accounts.config({
 });
 
 if (Meteor.isServer) {
+    Meteor.users._ensureIndex({ "profile.location":'2dsphere'});
     Meteor.methods({
         createTempUser: createTempUser,
     });
@@ -17,7 +18,7 @@ Meteor.methods({
 function createTempUser(name) {
     var uniqueEmail  = getUniqueEmail();
         credentials  = { email: uniqueEmail, username: uniqueEmail, password: uniqueEmail },
-        userFields   = _.extend(credentials, { profile: { name: name }});
+        userFields   = _.extend(credentials, { profile: { name: name,  location: null}});
     Accounts.createUser(userFields);
     return credentials;
 }
@@ -39,4 +40,8 @@ function getUniqueEmail() {
 
 function usernameExists(username) {
     return !! Meteor.users.findOne({ username: username });
+}
+
+function locToMongo(location){
+    return {type: 'Point', coordinates: [location.long, location.lat]};
 }
