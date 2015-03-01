a9f1458493d027cc781f0d95ed07877e2301773e
Seth Ruji <someoneseth@gmail.com>
2015-03-01 08:49:39 -0600

    destination marker displays

--- client/views/mapView/map.js
+++ client/views/mapView/map.js
@@ -17,26 +17,27 @@ Template.map.created = function() {
   GoogleMaps.ready('exampleMap', function(gmap) {	  			  	
   		var centerPoint = panCurrentLocation(gmap);
   		updateMarkers(gmap, centerPoint);	  				  		
-  		
+  		var centerPoint;
   		window.setInterval(function(){
-  			var centerPoint = panCurrentLocation(gmap);
+  			centerPoint = panCurrentLocation(gmap);
   			updateMarkers(gmap, centerPoint);	  				  		
   		}, 5000);	    
   });	  
 };	
 
 var panCurrentLocation = function(myMap){		
-var users= Meteor.users.find();
-var bounds = new google.maps.LatLngBounds();	
-var loc;
-users.forEach(function(user){
-	loc = user.profile.location.coordinates;
-	var gLatLng= new google.maps.LatLng(loc[0], loc[1]);
-	bounds.extend(gLatLng);
-})	
-	
-myMap.instance.fitBounds(bounds);	
-return bounds.getCenter();
+  var users= Meteor.users.find();
+  var bounds = new google.maps.LatLngBounds();	
+  var loc;
+  users.forEach(function(user){
+  	loc = user.profile.location.coordinates;
+  	var gLatLng= new google.maps.LatLng(loc[0], loc[1]);
+  	bounds.extend(gLatLng);
+  })	
+  	
+  myMap.instance.fitBounds(bounds);	
+  // console.log(bounds.getCenter());
+  return bounds.getCenter();
 }
 
 markers = [];
@@ -47,40 +48,45 @@ for(var i = 0; i < markers.length; i++) {
 }
 
 var updateMarkers= function(myMap, centerPoint){	
-var users= Meteor.users.find();
-var loc;
-var populationOptions = {
-    strokeColor: 'White',
-    strokeOpacity: 0.3,
-    strokeWeight: 2,
-    fillColor: 'Red',
-    fillOpacity: 0.3,
-    map: myMap.instance,      
-    radius: 3
-  };
-1
-clearMarkers();	
-  var i=0;
-users.forEach(function(user){
-	loc = user.profile.location.coordinates;
-	var gLatLng= new google.maps.LatLng(loc[0], loc[1]);
-	populationOptions.center= gLatLng;
-	var tempUser= Meteor.user();
-	if(tempUser._id == user._id){
-		populationOptions.fillColor = "Orange";
-		console.log("drawing user dot");
-	}
+  var users= Meteor.users.find();
+  var loc;
+  var populationOptions = {
+      strokeColor: 'White',
+      strokeOpacity: 0.3,
+      strokeWeight: 2,
+      fillColor: 'Red',
+      fillOpacity: 0.3,
+      map: myMap.instance,      
+      radius: 3
+    };
+
+  clearMarkers();	
+  
+  var flagLoc= new google.maps.LatLng(centerPoint.k, centerPoint.D);   
+  populationOptions.fillColor= 'Green';
+  populationOptions.center= flagLoc;   
+  console.log("drawing "+ populationOptions.fillColor+" dot at "+ populationOptions.center);
+  markers[0] = new google.maps.Circle(populationOptions);   
+
+  var i=1;
+  var gLatLng, tempUser;
+  populationOptions.fillColor= 'Red';
+  users.forEach(function(user){
+  	loc = user.profile.location.coordinates;
+  	gLatLng= new google.maps.LatLng(loc[0], loc[1]);
+    console.log(gLatLng);
+  	populationOptions.center= gLatLng;
+  	tempUser= Meteor.user();
+  	if(tempUser._id == user._id){
+  		populationOptions.fillColor = "Orange";  		
+  	}
 
   	// Add the circle for this city to the map.    	
-
-  	var circle = new google.maps.Circle(populationOptions);
-  	markers[i++]= circle;
+    console.log("drawing "+ populationOptions.fillColor+" dot at "+ populationOptions.center);
+  	markers[i++] = new google.maps.Circle(populationOptions);  	
   });
-  var flagLoc= new google.maps.LatLng(centerPoint); 
-  populationOptions.fillColor= 'Green';
-	populationOptions.center= flagLoc;   
-  var circle = new google.maps.Circle(populationOptions);
-  markers[i++] = circle;    
+
+   
 }
 // 	if(Meteor.user()==user){
 // 		var marker = new google.maps.Marker({
