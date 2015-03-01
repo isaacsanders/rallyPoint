a9ced1eb602cfbb3f42f5931c69d3b049a01e4d6
John McCormack <mccormjt@gmail.com>
2015-03-01 03:50:17 -0600

    added support for showing distance

--- client/views/friend_list/friend_list.js
+++ client/views/friend_list/friend_list.js
@@ -1,9 +1,14 @@
-if (Meteor.isClient) {
-	// This only runs on client
-	Template.friendList.helpers({
+Template.friendList.helpers({
 		friends: function () {
 			return Meteor.users.find({});
 		}
-	});
+});
 
-}
+
+Template.friend.helpers({
+    distance: function() {
+        var thierCoords = this.profile.location.coordinates;
+        var myCoords    = Meteor.user().profile.location.coordinates;
+        return Util.distance(thierCoords[0], thierCoords[1], myCoords[0], myCoords[1]).toFixed(2) + ' mi';
+    }
+});
